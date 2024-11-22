84ac19c70319fba2463938e279ca5642af11a3e9
Sandun Thisara <89610546+Studawatta@users.noreply.github.com>
2024-11-22 15:13:53 +0530

    complete profile page functionlities

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { updateUser } from '../controllers/user.controller.js';
+import {
+  updateUser,
+  deleteUser,
+  signout,
+} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.put('/update/:id', verifyToken, updateUser);
+router.delete('/delete/:id', verifyToken, deleteUser);
+router.post('/signout', signout);
 
 export default router;
