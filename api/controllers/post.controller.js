f7e46f45f29cf2de1c4a89d006404ba06d653ff4
Sandun Thisara <89610546+Studawatta@users.noreply.github.com>
2024-11-23 11:25:51 +0530

    create post api route

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,30 @@
+import Post from '../models/post.model.js';
+import { customError } from '../utils/error.js';
+
+export const createPost = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(customError(403, 'You are not allowed to create a post.'));
+  }
+
+  if (!req.body.title || !req.body.content) {
+    return next(customError(400, 'Please provide all required fields.'));
+  }
+  const slug = req.body.title
+    .split(' ')
+    .join('_')
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, '-');
+
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import { errorHandler } from './middlewares/error.js';
 
 mongoose
@@ -17,6 +18,7 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
 
 app.use(errorHandler);
 
