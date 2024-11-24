33271eace6f347853780ea24a0e6f3c79e521452
Sandun Thisara <89610546+Studawatta@users.noreply.github.com>
2024-11-24 18:31:47 +0530

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
+router.get('/', getPosts);
 router.post('/create', verifyToken, createPost);
 
 export default router;
