fea8560d150063452747a8bab044ab7d822d63e6
Sandun Thisara <89610546+Studawatta@users.noreply.github.com>
2024-11-25 20:56:24 +0530

    create get Users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,11 +3,13 @@ import {
   updateUser,
   deleteUser,
   signout,
+  getUsers,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
+router.get('/', verifyToken, getUsers);
 router.put('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
 router.post('/signout', signout);
